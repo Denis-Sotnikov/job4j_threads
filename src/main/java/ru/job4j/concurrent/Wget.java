e5009ba8ad1f31388ac6e592a8ec1934c0d63970
Denis Sotnikov <sport_piter@mail.ru>
2021-02-09 14:18:53 +0300

    4. Скачивание файла с ограничением. [#469190]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,19 +1,51 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread first = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i <= 100; i++) {
-                            System.out.print("\rLoading : " + i  + "%");
-                            Thread.sleep(1000);
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-        );
-        first.start();
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        String file = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            System.out.println("speed = " + this.speed);
+            int speedLimit = this.speed;
+            long pause = 0;
+            long timeFirst = new Date().getTime();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long result = new Date().getTime() - timeFirst - pause;
+                timeFirst = new Date().getTime();
+                pause = result < this.speed ? this.speed - result : 0;
+                System.out.println("result = " + result);
+                System.out.println("pause = " + pause);
+                Thread.sleep(pause);
+            }
+            System.out.println("Done");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
