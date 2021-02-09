ef14ccd64139cde92efedd84b7b2308de3e54ecb
Denis Sotnikov <sport_piter@mail.ru>
2021-02-09 03:48:21 +0300

    3. Прерывание нити [#469195]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,34 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(1); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        String[] array = {"\\", "|", "/"};
+        int count = 0;
+        for (int i = 0; i <= 100; i++) {
+            if (!Thread.currentThread().isInterrupted()) {
+                try {
+                    if (count == 3) {
+                        count = 0;
+                    }
+                    System.out.print("\r load: " + "Loading ..... " + array[count]);
+                    Thread.sleep(500);
+                    count++;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
