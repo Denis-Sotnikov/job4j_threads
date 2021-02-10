ff41facf496756b1a7533057977ed81f6be3bbad
Denis Sotnikov <sport_piter@mail.ru>
2021-02-10 06:03:30 +0300

    1.1. Состояние нити. [#469191]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,17 +3,36 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) throws InterruptedException {
         Thread first = new Thread(
-                () -> System.out.println(Thread.currentThread().getName())
+                () -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName());
+                }
         );
 
         Thread second = new Thread(
-                () -> System.out.println(Thread.currentThread().getName())
+                () -> {
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName());
+                }
         );
         first.start();
 //        first.join();
         second.start();
 //        second.join();
-        Thread.sleep(1000);
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " " + first.getState());
+        }
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+        }
         System.out.println("Work done");
     }
 
