f8d16c4b8db112872be107a7ef76eab8c162fbd5
Denis Sotnikov <sport_piter@mail.ru>
2021-02-11 11:24:26 +0300

    1. Visibility. Общий ресурс вне критической секции [#469205]

---
+++ src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -0,0 +1,42 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+
+public class ParseFile {
+    private volatile File file;
+
+    public synchronized void setFile(File f) {
+        file = f;
+    }
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public synchronized String getContent() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        StringBuilder output = null;
+        while (reader.ready()) {
+            output.append(reader.readLine());
+        }
+        return output.toString();
+    }
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        InputStream i = new FileInputStream(file);
+        StringBuilder output = null;
+        int data;
+        while ((data = i.read()) > 0) {
+            if (data < 128) {
+                output.append((char) data);
+            }
+        }
+        return output.toString();
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        OutputStream o = new FileOutputStream(file);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(content);
+    }
+}
