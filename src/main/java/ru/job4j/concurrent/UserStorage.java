4d3c8201dac2103426ff8f7ad9b00a9295795fbe
Denis Sotnikov <sport_piter@mail.ru>
2021-02-12 04:19:58 +0300

    3. Класс хранилища пользователей UserStorage [#469206]

--- src/main/java/ru/job4j/concurrent/UserStorage.java
+++ src/main/java/ru/job4j/concurrent/UserStorage.java
@@ -8,22 +8,31 @@ import java.util.*;
 @ThreadSafe
 public class UserStorage {
     @GuardedBy("this")
-    Map<Integer, User> mapUsers = new HashMap<>();
+    private final Map<Integer, User> mapUsers = new HashMap<>();
 
     public synchronized void transfer(int fromId, int toId, int amount) {
-        User first = mapUsers.get(fromId);
-        User second = mapUsers.get(toId);
-        first.setAmount(first.getAmount() - amount);
-        second.setAmount(second.getAmount() + amount);
+        if (mapUsers.get(fromId) != null && mapUsers.get(toId) != null) {
+            User first = mapUsers.get(fromId);
+            User second = mapUsers.get(toId);
+            if (first.getAmount() >= amount) {
+                first.setAmount(first.getAmount() - amount);
+                second.setAmount(second.getAmount() + amount);
+                System.out.println("Транзакция исполнена");
+            } else {
+                System.out.println("Транзакция не исполнена");
+            }
+        } else {
+            System.out.println("Транзакция не исполнена");
+        }
     }
 
     public synchronized boolean add(User user) {
-        return mapUsers.put(user.getId(), user) != null;
+        return mapUsers.putIfAbsent(user.getId(), user) == null;
     }
     public synchronized boolean update(User user) {
-        return mapUsers.put(user.getId(), user) != null;
+        return mapUsers.replace(user.getId(), user) != null;
     }
     public synchronized boolean delete(User user) {
-        return mapUsers.remove(user.getId()) != null;
+        return mapUsers.remove(user.getId(), user);
     }
 }
