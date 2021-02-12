69209df61d10ed67aa87093db98a52e94735eecb
Denis Sotnikov <sport_piter@mail.ru>
2021-02-12 05:14:53 +0300

    2. Обеспечить остановку потребителя. [#469185]

---
+++ src/main/java/ru/job4j/concurrent/ParallelSearch.java
@@ -0,0 +1,40 @@
+package ru.job4j.concurrent;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        try {
+                            Integer val = queue.poll();
+                            if (val == 2) {
+                                System.out.println(val);
+                                break;
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).start();
+    }
+}
