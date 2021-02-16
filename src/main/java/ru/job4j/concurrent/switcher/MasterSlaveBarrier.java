cad3b893d515d95759567938a6bbf8e18217f60e
Denis Sotnikov <sport_piter@mail.ru>
2021-02-16 04:47:44 +0300

    2. ExecutorService рассылка почты. [#469179]

---
+++ src/main/java/ru/job4j/concurrent/switcher/MasterSlaveBarrier.java
@@ -0,0 +1,92 @@
+package ru.job4j.concurrent.switcher;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class MasterSlaveBarrier {
+
+    /**
+     * Master
+     */
+    @GuardedBy("this")
+    private boolean master = true;
+
+    /**
+     * Slave
+     */
+    @GuardedBy("this")
+    private boolean slave = false;
+
+    /**
+     * Method of run master
+     */
+    public synchronized void tryMaster() {
+        while (isMaster()) {
+            System.out.println("Thread A");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+        notifyAll();
+    }
+
+    /**
+     * Method of run slave
+     */
+    public synchronized void trySlave() {
+        if (isMaster()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+        setSlave(true);
+        while (isSlave()) {
+            System.out.println("Thread B");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    /**
+     * Method of done master
+     * sends notifyAll()
+     */
+    public void doneMaster() {
+        setMaster(false);
+    }
+
+    /**
+     * Method of done slave
+     * interrupt current thread
+     */
+    public void doneSlave() {
+        setSlave(false);
+    }
+
+    public synchronized boolean isMaster() {
+        return master;
+    }
+
+    public void setMaster(boolean master) {
+        this.master = master;
+    }
+
+    public synchronized boolean isSlave() {
+        return slave;
+    }
+
+    public void setSlave(boolean slave) {
+        this.slave = slave;
+    }
+}
